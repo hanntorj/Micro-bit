f39c5a2d29e5f0af91e2d2a53406740a87a10473
Your Name <you@example.com>
2019-02-19 16:05:26 +0100

    Dag 2

---
+++ 2_uart/main.c
@@ -0,0 +1,40 @@
+#include "uart.h"
+#include <stdint.h>
+#include "gpio.h"
+
+int main(){
+	// Configure LED Matrix
+	for(int i = 4; i <= 15; i++){
+		GPIO->DIRSET = (1 << i);
+		GPIO->OUTCLR = (1 << i);
+	}
+
+	// Configure buttons
+	GPIO->PIN_CNF[17] = 0;
+	GPIO->PIN_CNF[26] = 0;
+
+	//GPIO->OUTSET=(1<<13); 				//Her lærte vi å få radene til å lyse
+
+    uart_init();
+    int sleep = 0;
+	while(1){
+
+		if(!(GPIO->IN & (1<<26))){			// Check if button B is pressed
+			for(int i = 13; i <= 15; i++){
+				GPIO->OUTSET = (1 << i);	//turn on LED matrix if it is.
+            }
+            uart_send('B');
+		}
+
+		if(!(GPIO->IN & (1<<17))){			// Check if button A is pressed;
+			for(int i=13; i <=15; i++){
+				GPIO->OUTCLR=(1<<i);		//turn off LED matrix if it is.
+        	}
+            uart_send('A');
+		}
+		sleep = 10000;
+		while(--sleep);
+	}
+	return 0;
+}
+ /* trykk bokstav på tastaturet istedenfor på micro:biten 
